b1f09c8ea1c16a33c51c793ccb422de4b4d68228
floation_cutie <wzxFloation_cutie@bupt.edu.cn>
2025-10-04 21:25:37 +0800

    feat: support generic csv header data

--- src/process.rs
+++ src/process.rs
@@ -2,10 +2,12 @@ use std::path::PathBuf;
 
 use csv::Reader;
 use serde::{Deserialize, Serialize};
-use serde_json::to_string_pretty;
+use serde_json::{Value, to_string_pretty};
 
+const CSV_CAPACITY: usize = 128;
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
+#[allow(unused)]
 struct Player {
     name: String,
     position: String,
@@ -18,21 +20,17 @@ struct Player {
 
 pub fn process_csv(input: &PathBuf, output: &PathBuf) -> anyhow::Result<()> {
     let mut reader = Reader::from_path(input).expect("Failed to open CSV file");
-    /*
-    let mut _record = Vec::new();
-    // Since it is a borrowed iterator, the reader itself didn't move. However
-    for result in reader.deserialize(){
-        let record: Player = result.expect("Failed to deserialize record");
-        // println!("{:?}", record);
-        _record.push(record);
+
+    let mut ret = Vec::with_capacity(CSV_CAPACITY);
+    let headers = reader.headers().expect("Failed to read headers").clone();
+    // support more generic csv files
+    for record in reader.records() {
+        let record = record.expect("Failed to read record");
+        let json_value = headers.iter().zip(record.iter()).collect::<Value>();
+        ret.push(json_value);
     }
-    */
-    let records = reader
-        .deserialize()
-        .map(|result| result.expect("Failed to deserialize record"))
-        .collect::<Vec<Player>>();
-    println!("{:?}", records);
-    let json = to_string_pretty(&records).expect("Failed to serialize to JSON");
+
+    let json = to_string_pretty(&ret).expect("Failed to serialize to JSON");
     std::fs::write(output, json).expect("Failed to write JSON file");
     Ok(())
 }
